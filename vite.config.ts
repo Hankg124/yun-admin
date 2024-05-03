48bef193ce5c30b8d2deb6c3191153504b313be8
hkg <1416737291@qq.com>
2024-05-03 13:58:06 +0800

    配置端口

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  server: {
+    //配置端口号
+    port: 8888,
+    //配置自动打开浏览器
+    open:true,
+    //主机名
+    host:'localhost',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
