608eb3216fb11c868a111368a07ecdddad635c40
hkg <1416737291@qq.com>
2024-05-08 21:02:26 +0800

    P11方式实现面包屑及右侧下拉菜单布局

--- src/layout/layoutHeader/Breadcrumb.vue
+++ src/layout/layoutHeader/Breadcrumb.vue
@@ -4,41 +4,73 @@
         <svg-icon @click="isCollapseFn" class="svgicon" :icon="stores.isCollapse ? 'Fold' : 'Expand'" size="18" />
         <!-- 面包屑 -->
         <el-breadcrumb separator="/">
-            <el-breadcrumb-item v-for="item in route.matched" :key="item.path">
-                <div class="el-breadcrumb-item-div" v-if="!item.children">
+            <el-breadcrumb-item v-for="(item, index) in breadcrumbList" :key="index">
+                <div class="el-breadcrumb-item-div" v-if="breadcrumbList!.length == 0">
+                    <svg-icon :icon="item.meta.icon"></svg-icon>
                     <span>{{ item.meta.title }}</span>
                 </div>
-                <a @click="breadcrumbFn(item)">{{ item.meta.title }}</a>
+                <a class="el-breadcrumb-item-div" @click="breadcrumbFn(item)">
+                <svg-icon :icon="item.meta.icon"></svg-icon>
+                {{ item.meta.title }}</a>
+
             </el-breadcrumb-item>
         </el-breadcrumb>
     </div>
 </template>
 <script setup lang='ts'>
-import { useRoute, useRouter } from 'vue-router'
+import {
+    useRoute,
+    useRouter,
+    onBeforeRouteUpdate,
+    type RouteLocationNormalized,
+    type RouteRecordNormalized
+} from 'vue-router'
 const route = useRoute();
 const router = useRouter();
-console.log(route.matched);
+
 
 import { useLayoutConfigStore } from "@/stores/layoutConfig.ts"
+import { onMounted, ref } from 'vue';
 const stores = useLayoutConfigStore();
-
-const breadcrumbFn = (item: any) => {
-    console.log(item);
-
-    if (item.redirect) {
-        router.push(item.redirect);
-    } else {
-        router.push(item.path);
-    }
-}
 
 const isCollapseFn = () => {
     stores.isCollapse = !stores.isCollapse;
     console.log(stores.isCollapse);
 }
 
+//创建一个变量，保存面包屑导航的数据
+const breadcrumbList = ref<RouteRecordNormalized[]>();
 
 
+//路由更新时触发的方法
+onBeforeRouteUpdate((to, from) => {
+    getBreadcrumb(to);
+})
+
+//定义一个方法，使用这个方法获取路由跳转的信息
+const getBreadcrumb = (to: RouteLocationNormalized) => {
+    console.log("to", to);
+    const matched = to.matched.filter((item) => item.meta && item.meta.title && item.meta.icon)
+    // console.log("matched",matched);
+    breadcrumbList.value = matched;
+    console.log(breadcrumbList.value);
+}
+
+onMounted(() => {
+    getBreadcrumb(route);
+})
+
+
+const breadcrumbFn = (_route:RouteRecordNormalized) => {
+    console.log(_route);
+
+    const {path,redirect}=_route;
+
+    if (redirect) router.push(redirect as string);
+               // router.push(<string>redirect);
+    else router.push(path);
+    
+}
 
 
 </script>
--- src/layout/layoutHeader/UserDropdown.vue
+++ src/layout/layoutHeader/UserDropdown.vue
@@ -1,12 +1,40 @@
 <template>
-<div>
-    right
-</div>
+    <div class="layout-header-user">
+        <!-- 全屏 -->
+        <div class="layout-header-user-icon">
+            <svg-icon icon="FullScreen" />
+        </div>
+        <!-- 开关 -->
+        <div class="layout-header-user-icon">
+            <el-switch></el-switch>
+        </div>
+        <!-- 下拉菜单 -->
+        <div>
+            <el-dropdown @command="handleCommand">
+                <span class="el-dropdown-link">
+                    <el-avatar :size="30" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" />
+                    <span>乔芳</span>
+                    <el-icon class="el-icon--right"><arrow-down /></el-icon>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item command="首页">首页</el-dropdown-item>
+                        <el-dropdown-item command="404">404</el-dropdown-item>
+                        <el-dropdown-item command="401">401</el-dropdown-item>
+                        <el-dropdown-item command="退出系统" divided>退出系统</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </div>
+    </div>
 </template>
 <script setup lang='ts'>
+import { ElMessage } from 'element-plus'
+import { ArrowDown } from '@element-plus/icons-vue'
 
+const handleCommand = (command: string | number | object) => {
+    ElMessage(`click on item ${command}`)
+}
 
 </script>
-<style lang='scss' scoped>
-
-</style>
+<style lang='scss' scoped></style>
