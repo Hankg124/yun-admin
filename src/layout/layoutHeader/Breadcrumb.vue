3e39eb5251adf6d0a425947c9ae4f2e25341127b
hkg <1416737291@qq.com>
2024-05-08 16:56:18 +0800

    使用P10的方式实现面包屑

--- src/layout/layoutHeader/Breadcrumb.vue
+++ src/layout/layoutHeader/Breadcrumb.vue
@@ -3,7 +3,14 @@
         <!-- 控制 菜单栏 展开/收起 -->
         <svg-icon @click="isCollapseFn" class="svgicon" :icon="stores.isCollapse ? 'Fold' : 'Expand'" size="18" />
         <!-- 面包屑 -->
-        
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item v-for="item in route.matched" :key="item.path">
+                <div class="el-breadcrumb-item-div" v-if="!item.children">
+                    <span>{{ item.meta.title }}</span>
+                </div>
+                <a @click="breadcrumbFn(item)">{{ item.meta.title }}</a>
+            </el-breadcrumb-item>
+        </el-breadcrumb>
     </div>
 </template>
 <script setup lang='ts'>
@@ -12,9 +19,18 @@ const route = useRoute();
 const router = useRouter();
 console.log(route.matched);
 
-
 import { useLayoutConfigStore } from "@/stores/layoutConfig.ts"
 const stores = useLayoutConfigStore();
+
+const breadcrumbFn = (item: any) => {
+    console.log(item);
+
+    if (item.redirect) {
+        router.push(item.redirect);
+    } else {
+        router.push(item.path);
+    }
+}
 
 const isCollapseFn = () => {
     stores.isCollapse = !stores.isCollapse;
